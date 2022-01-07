5295748edc97a7534325a616d563427c3a7d0faf
noor848 <54281955+noor848@users.noreply.github.com>
2022-01-07 16:04:41 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+
+import 'package:flutter/cupertino.dart';
+
+void main() {
+  runApp();
+}
+
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    
+    return build(context)
+    
+    
+  }
+  
+  
+  
+  
+  
+  
+}
