f1b2afdf1bb3fe284a353cd59daf4194e06bfce3
noor848 <54281955+noor848@users.noreply.github.com>
2022-01-13 18:32:22 +0200

    adding main

---
+++ lib/module/counter/main.dart
@@ -0,0 +1,42 @@
+
+import 'package:bloc/bloc.dart';
+import 'package:bma/counter.dart';
+import 'package:bma/module/counter/cubit.dart';
+import 'package:bma/module/counter/statecounter.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_services_binding/flutter_services_binding.dart';
+import 'BMI_cal.dart';
+import 'login_page.dart';
+import 'module/square_rotate/square.dart';
+import 'shared/blocobserver.dart';
+
+void main() {
+
+  FlutterServicesBinding.ensureInitialized();
+  BlocOverrides.runZoned(
+          () {
+        runApp(const MyApp());
+      },
+      blocObserver: MyBlocObserver(),
+    );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+      debugShowCheckedModeBanner: false,
+      home: Counter(),
+
+
+
+    );
+  }
+}
+
+
+
